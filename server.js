61829e9efebf084a9f35be2658c3601f412a3ab9
LmnopEmc2 <110025657+LmnopEmc2@users.noreply.github.com>
2025-01-22 00:35:53 +0330

    projects github links added. express code added

--- server.js
+++ server.js
@@ -1,58 +1,13 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : req.url
-  );
+const port = 5000;
 
-  let extName = path.extname(filePath);
+app.use(express.static("./public"));
 
-  let contentType = "text/html";
-
-  switch (extName) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpg";
-      break;
-  }
-
-  if (contentType === "text/html" && extName === "") {
-    filePath += ".html";
-  }
-  // console.log(filePath);
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      // console.log(err);
-      // throw err;
-      if (err.code === "ENOENT") {
-        fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end(data);
-        });
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(data, "utf-8");
-    }
-  });
+app.get("/", (req, res) => {
+  // res.sendFile("./public/index.html");
+  return;
 });
 
-server.listen(5000, () => console.log("listenning"));
+app.listen(5000, console.log(`listenning on port ${port}`));
